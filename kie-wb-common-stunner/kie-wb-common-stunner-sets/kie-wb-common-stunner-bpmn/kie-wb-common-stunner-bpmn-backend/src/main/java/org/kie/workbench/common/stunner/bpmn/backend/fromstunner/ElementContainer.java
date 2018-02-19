e719340a8cbe395db27dc9dd8a033ffd90b521ee
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-02-19 14:54:51 +0100

    initial suport to subprocess

---
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/fromstunner/ElementContainer.java
@@ -0,0 +1,5 @@
+package org.kie.workbench.common.stunner.bpmn.backend.fromstunner;
+
+public interface ElementContainer {
+
+}
---
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/fromstunner/processes/SubProcessConverter.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.stunner.bpmn.backend.fromstunner.processes;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.bpmn2.Process;
+import org.kie.workbench.common.stunner.bpmn.backend.converters.Result;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.DefinitionsBuildingContext;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.SequenceFlowConverter;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.ViewDefinitionConverter;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.lanes.LaneConverter;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.properties.ActivityPropertyWriter;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.properties.BasePropertyWriter;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.properties.BoundaryEventPropertyWriter;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.properties.LanePropertyWriter;
+import org.kie.workbench.common.stunner.bpmn.backend.fromstunner.properties.ProcessPropertyWriter;
+import org.kie.workbench.common.stunner.bpmn.definition.BPMNDiagramImpl;
+import org.kie.workbench.common.stunner.bpmn.definition.property.diagram.DiagramSet;
+import org.kie.workbench.common.stunner.bpmn.definition.property.variables.ProcessData;
+import org.kie.workbench.common.stunner.core.graph.Node;
+import org.kie.workbench.common.stunner.core.graph.content.definition.Definition;
+
+import static org.kie.workbench.common.stunner.bpmn.backend.fromstunner.Factories.bpmn2;
+
+public class ProcessConverter {
+
+    private final DefinitionsBuildingContext context;
+
+    private final SequenceFlowConverter sequenceFlowConverter;
+    private final ViewDefinitionConverter viewDefinitionConverter;
+    private final LaneConverter laneConverter;
+
+    public ProcessConverter(DefinitionsBuildingContext context) {
+        this.context = context;
+        this.sequenceFlowConverter = new SequenceFlowConverter(context);
+        this.viewDefinitionConverter = new ViewDefinitionConverter();
+        this.laneConverter = new LaneConverter();
+    }
+
+    public ProcessPropertyWriter toFlowElement(Node<Definition<BPMNDiagramImpl>, ?> node) {
+        ProcessPropertyWriter p = convertProcessNode(node);
+
+        context.nodes()
+                .map(viewDefinitionConverter::toFlowElement)
+                .filter(Result::notIgnored)
+                .map(Result::value)
+                .forEach(p::addChildElement);
+
+        List<LanePropertyWriter> lanes = context.nodes()
+                .map(laneConverter::toElement)
+                .filter(Result::notIgnored)
+                .map(Result::value)
+                .collect(Collectors.toList());
+
+        p.addLaneSet(lanes);
+        lanes.forEach(p::addChildElement);
+
+        context.childEdges()
+                .forEach(e -> {
+                    BasePropertyWriter pSrc = p.getChildElement(e.getSourceNode().getUUID());
+                    // if it's null, then it's a root: skip it
+                    if (pSrc != null) {
+                        BasePropertyWriter pTgt = p.getChildElement(e.getTargetNode().getUUID());
+                        pTgt.setParent(pSrc);
+                    }
+                });
+
+        context.dockEdges()
+                .forEach(e -> {
+                    ActivityPropertyWriter pSrc =
+                            (ActivityPropertyWriter) p.getChildElement(e.getSourceNode().getUUID());
+                    BoundaryEventPropertyWriter pTgt =
+                            (BoundaryEventPropertyWriter) p.getChildElement(e.getTargetNode().getUUID());
+
+                    pTgt.setParentActivity(pSrc);
+                });
+
+        context.edges()
+                .map(e -> sequenceFlowConverter.toFlowElement(e, p))
+                .forEach(p::addChildElement);
+
+        return p;
+    }
+
+    private ProcessPropertyWriter convertProcessNode(Node<Definition<BPMNDiagramImpl>, ?> node) {
+        Process process = bpmn2.createProcess();
+
+        ProcessPropertyWriter p = new ProcessPropertyWriter(process);
+        BPMNDiagramImpl definition = node.getContent().getDefinition();
+
+        DiagramSet diagramSet = definition.getDiagramSet();
+
+        p.setName(diagramSet.getName().getValue());
+        p.setDocumentation(diagramSet.getDocumentation().getValue());
+
+        process.setId(diagramSet.getId().getValue());
+        p.setPackage(diagramSet.getPackageProperty().getValue());
+        p.setVersion(diagramSet.getVersion().getValue());
+        p.setAdHoc(diagramSet.getAdHoc().getValue());
+        p.setDescription(diagramSet.getProcessInstanceDescription().getValue());
+        p.setExecutable(diagramSet.getExecutable().getValue());
+
+        ProcessData processData = definition.getProcessData();
+        p.setProcessVariables(processData.getProcessVariables());
+
+        p.setSimulationSet(null); // fixme: inserting default data
+        return p;
+    }
+}
