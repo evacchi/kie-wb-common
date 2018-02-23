18481cc19cd1c95c2925e9114562805fe3bf41b5
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-02-23 12:00:40 +0100

    attribute dsl

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/converters/properties/Attribute.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/converters/properties/Attribute.java
@@ -6,10 +6,12 @@ import org.kie.workbench.common.stunner.bpmn.definition.property.diagram.Package
 public class Attribute<T> {
 
     public static final AttributeDefinition<Boolean> independent = new BooleanAttribute("independent", false);
+    public static final AttributeDefinition<Boolean> adHoc = new BooleanAttribute("adHoc", false);
     public static final AttributeDefinition<Boolean> waitforCompletion = new BooleanAttribute("waitforCompletion", false);
     public static final AttributeDefinition<String> dockerInfo = new StringAttribute("dockerInfo", "");
     public static final AttributeDefinition<String> ruleFlowGroup = new StringAttribute("ruleFlowGroup", "");
     public static final AttributeDefinition<String> packageName = new StringAttribute("packageName", Package.DEFAULT_PACKAGE);
+    public static final AttributeDefinition<String> version = new StringAttribute("version", "1.0");
 
     private final AttributeDefinition<T> attributeDefinition;
     private final BaseElement element;
@@ -21,5 +23,9 @@ public class Attribute<T> {
 
     public T get() {
         return attributeDefinition.getValue(element);
+    }
+
+    public void set(T value) {
+        attributeDefinition.setValue(element, value);
     }
 }
--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/converters/properties/AttributeDefinition.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/converters/properties/AttributeDefinition.java
@@ -3,12 +3,15 @@ package org.kie.workbench.common.stunner.bpmn.backend.converters.properties;
 import java.util.Optional;
 
 import org.eclipse.bpmn2.BaseElement;
+import org.eclipse.emf.ecore.impl.EAttributeImpl;
+import org.eclipse.emf.ecore.impl.EStructuralFeatureImpl;
+
+import static org.kie.workbench.common.stunner.bpmn.backend.fromstunner.Factories.metaData;
 
 public abstract class AttributeDefinition<T> {
 
     private final String name;
     protected final T defaultValue;
-    private T value;
 
     public AttributeDefinition(String name, T defaultValue) {
         this.name = name;
@@ -21,11 +24,26 @@ public abstract class AttributeDefinition<T> {
 
     public abstract T getValue(BaseElement element);
 
+    public abstract void setValue(BaseElement element, T value);
+
     public Optional<java.lang.String> getStringValue(BaseElement element) {
         return element.getAnyAttribute().stream()
                 .filter(e -> this.name().equals(e.getEStructuralFeature().getName()))
                 .map(e -> e.getValue().toString())
                 .findFirst();
+    }
+
+    public void setStringValue(BaseElement element, String value) {
+        EAttributeImpl extensionAttribute = (EAttributeImpl) metaData.demandFeature(
+                "http://www.jboss.org/drools",
+                name,
+                false,
+                false);
+
+        EStructuralFeatureImpl.SimpleFeatureMapEntry feature =
+                new EStructuralFeatureImpl.SimpleFeatureMapEntry(extensionAttribute, value);
+
+        element.getAnyAttribute().add(feature);
     }
 
     public Attribute<T> of(BaseElement element) {
@@ -45,6 +63,11 @@ class BooleanAttribute extends AttributeDefinition<Boolean> {
                 .map(java.lang.Boolean::parseBoolean)
                 .orElse(defaultValue);
     }
+
+    @Override
+    public void setValue(BaseElement element, Boolean value) {
+        setStringValue(element, String.valueOf(value));
+    }
 }
 
 class StringAttribute extends AttributeDefinition<String> {
@@ -57,5 +80,10 @@ class StringAttribute extends AttributeDefinition<String> {
     public java.lang.String getValue(BaseElement element) {
         return getStringValue(element)
                 .orElse(defaultValue);
+    }
+
+    @Override
+    public void setValue(BaseElement element, String value) {
+        setStringValue(element, value);
     }
 }
